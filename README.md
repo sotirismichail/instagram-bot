bfec9ff302f2d975cd4b0da038dea931b949e6ba
Sotiris Michail <s.michail@sphynx.ch>
2023-04-07 12:27:35 +0300

    refactor: lint and type hint

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # instagram-bot
 An instagram bot using the Selenium framework, automating various tasks on instagram. Developed as part of the Advanced Secure Systems course of the Technical University of Crete.
+
+## Getting Started
+
+### Prerequisites
+
+The bot depends on the selenium web driver framework. To install the dependencies of the project, simply run:
+
+```sh
+pip install -r requirements.txt
+```
+
+Please note that the bot has been developed to be used on Firefox/Gecko-based browsers. Future plans include implementing the bot functionality for Chromium-based browsers.
+
+### Usage
+
+```sh
+python3 socialMediaLikeBot.py [username] [password] [profile] [posts]
+```
+
+To use the bot, you need to setup an account beforehand for the bot to use, in order for it to perform the "likes" on the target public profile. The "posts" parameter is the number of posts of the public profile to be "liked" by the bot. If a number greater than the number of total posts on the user profile is provided, the bot "likes" all of the public profile's posts.
+
+Should the target profile not be public, the bot will stop at the private profile's page.
+
+### Implementation Details
+
+The action targets for the bot are set with classes, tags and XPATH parameters. The bot locates the necessary buttons to perform the login process using the supplied user credentials, passes the cookie prompt, and after that, using the target profile parameter, the web driver follows the profile URL to perform the number of "likes" the user wants.
+
+ 
--- socialMediaLikeBot.py
+++ socialMediaLikeBot.py
@@ -10,94 +10,135 @@ from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.remote.webelement import WebElement
 
-def getRandomTime():
-        randTime = randint(3, 5)
-        return randTime
 
-def loggin(bot_name,bot_pass):
+def getRandomTime(None) -> int:
+    randTime = randint(3, 5)
+    return randTime
 
-    wait.until(EC.presence_of_element_located((By.NAME,"username"))).send_keys(bot_name)
-    wait.until(EC.presence_of_element_located((By.NAME,"password"))).send_keys(bot_pass)
-	# Click on the facebook log-in button
-    button = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")))
+
+def login(bot_name: str, bot_pass: str) -> None:
+    wait.until(EC.presence_of_element_located((By.NAME, "username"))).send_keys(
+        bot_name
+    )
+    wait.until(EC.presence_of_element_located((By.NAME, "password"))).send_keys(
+        bot_pass
+    )
+    # Click on the facebook log-in button
+    button = wait.until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button",
+            )
+        )
+    )
     browser.execute_script("arguments[0].click();", button)
-	# Save your login info : no
-    wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div"))).click()
-	# Turn off notifications
-    wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]"))).click()
-# def loggout():
+    # Save your login info : no
+    wait.until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div",
+            )
+        )
+    ).click()
+    # Turn off notifications
+    wait.until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]",
+            )
+        )
+    ).click()
 
-def get_number_of_posts():
-        """
-        Returns number of post for an account or tag
-        """
-        try:
-            num_of_posts = wait.until(EC.presence_of_element_located((By.XPATH, '//span[@class="_ac2a"]'))).text
-            num_of_posts = num_of_posts.replace(',','')
-            return int(num_of_posts)
-        except:
-            return None
+
+# def logout():
 
 
-def click_first_post():
-        """
-        Clicks on the first post found for an account
-        """
-        try:
-            #browser.wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="v1Nh3 kIKUG _bz0w"]'))).click()
-            wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="_aagw"]'))).click()
-            return True
-        except:
-            return False
+def get_number_of_posts(None) -> int:
+    """
+    Returns number of post for an account or tag
+    """
+    try:
+        num_of_posts = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//span[@class="_ac2a"]'))
+        ).text
+        num_of_posts = num_of_posts.replace(",", "")
+        return int(num_of_posts)
+    except:
+        return None
 
-def open_target(browser,targetName):
-        """
-        Opens the target account or hashtag
-        """
-        target="https://www.instagram.com/"+targetName
-        try:
-            browser.get(target)
-            # if unable to load the page
-            if not self.is_page_loaded():
-                logger.info("[open_target] Unable to load the page. Retrying...")
-                time.sleep(1)
-                return False
 
-            # if not a valid account or tag
-            elif not self.validate_target():
-                return 'skip_retry'
-        except:
-            return False
+def click_first_post(None) -> bool:
+    """
+    Clicks on the first post found for an account
+    """
+    try:
+        # browser.wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="v1Nh3 kIKUG _bz0w"]'))).click()
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, '//div[@class="_aagw"]'))
+        ).click()
         return True
+    except:
+        return False
 
-def like():
-        """
-        Likes a post if not liked already
-        """
-        like_button:WebElement = None
-        try:
-            like_button = wait.until(EC.presence_of_element_located((By.XPATH, '//span[@class="_aamw"]/button')))
-            like_button_span = like_button.find_element(By.XPATH, 'div/span')
-            button_status = like_button_span.find_element(By.TAG_NAME, 'svg').get_attribute('aria-label')
-            # like only if not already liked
-            if button_status == 'Like':
-                like_button.click()
 
-        except ElementClickInterceptedException:
-            browser.execute_script('arguments[0].click();', like_button)         
-        except:
-            print("Error mate")
-
-def next_post(browser): 
-        """
-        Moves to the next post
-        """
-        try:
-            wait.until(EC.presence_of_element_located((By.TAG_NAME,"body"))).send_keys(Keys.RIGHT)
-            return True
-        except:
+def open_target(browser: object, targetName: str) -> bool:
+    """
+    Opens the target account or hashtag
+    """
+    target = "https://www.instagram.com/" + targetName
+    try:
+        browser.get(target)
+        # if unable to load the page
+        if not self.is_page_loaded():
+            logger.info("[open_target] Unable to load the page. Retrying...")
+            time.sleep(1)
             return False
 
+        # if not a valid account or tag
+        elif not self.validate_target():
+            return "skip_retry"
+    except:
+        return False
+    return True
+
+
+def like(None) -> None:
+    """
+    Likes a post if not liked already
+    """
+    like_button: WebElement = None
+    try:
+        like_button = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//span[@class="_aamw"]/button'))
+        )
+        like_button_span = like_button.find_element(By.XPATH, "div/span")
+        button_status = like_button_span.find_element(By.TAG_NAME, "svg").get_attribute(
+            "aria-label"
+        )
+        # like only if not already liked
+        if button_status == "Like":
+            like_button.click()
+
+    except ElementClickInterceptedException:
+        browser.execute_script("arguments[0].click();", like_button)
+    except:
+        print("ERROR: Failed to perform like")
+
+
+def next_post(browser: object) -> bool:
+    """
+    Moves to the next post
+    """
+    try:
+        wait.until(EC.presence_of_element_located((By.TAG_NAME, "body"))).send_keys(
+            Keys.RIGHT
+        )
+        return True
+    except:
+        return False
 
 
 ## Starting the Bot
@@ -106,35 +147,35 @@ def next_post(browser):
 browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
 # ======= Setting =============
 # Your Facebook credentials
-bot_username = sys.argv[1] #"iordanispapavlou"
-bot_password = sys.argv[2] #"IpaP123"
+bot_username = sys.argv[1]  # "iordanispapavlou"
+bot_password = sys.argv[2]  # "IpaP123"
 targetName = sys.argv[3]
 numberOfPosts = int(sys.argv[4])
 
 # =============================
 # Open the Website
-browser.get('https://www.instagram.com/')
+browser.get("https://www.instagram.com/")
 wait = WebDriverWait(browser, timeout=50)
-wait.until(EC.presence_of_element_located((By.XPATH, "//button[text()='Only allow essential cookies']"))).click()
+wait.until(
+    EC.presence_of_element_located(
+        (By.XPATH, "//button[text()='Only allow essential cookies']")
+    )
+).click()
 
-# loggin in instagram
-loggin(bot_username,bot_password);
+# Log into instagram
+login(bot_username, bot_password)
 
-
-
-# Make a like
-open_target(browser,targetName)
+# Perform a like
+open_target(browser, targetName)
 maxPosts = get_number_of_posts()
 
-if maxPosts < numberOfPosts :
+if maxPosts < numberOfPosts:
     numberOfPosts = maxPosts
 
-if numberOfPosts != 0 :
+if numberOfPosts != 0:
     click_first_post()
     like()
 
-    for x in range(numberOfPosts-1):
+    for x in range(numberOfPosts - 1):
         next_post(browser)
         like()
-    
-
